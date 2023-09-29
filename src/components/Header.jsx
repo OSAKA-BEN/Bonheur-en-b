e8338c3df130eab21693b3f4e2e2d7efbbd3aac5
benjamin guiganton <benjamin.guiganton@gmail.com>
2023-09-29 15:39:45 +0900

    menu burger

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,11 @@ import { useContext, useEffect, useState } from "react";
 import DarkModeContext from "../DarkModeContext";
 
 const Header = () => {
+  // eslint-disable-next-line no-unused-vars
   const { darkMode, setDarkMode } = useContext(DarkModeContext);
   const [isScrolled, setIsScrolled] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
 
   useEffect(() => {
     const onScroll = () => {
@@ -17,11 +20,11 @@ const Header = () => {
   }, []);
 
   return (
-  <section id="header" style={{ backgroundImage: `url('src/assets/phoque-baie-de-somme.jpg')`, backgroundSize: 'cover', height: '100vh' }}>
+  <section id="header" style={{ backgroundImage: `url('src/assets/phoque-baie-de-somme.jpg')`, backgroundSize: 'cover', backgroundPosition: 'center', height: '100vh' }}>
   {/* Navbar */}
     <nav className={`fixed top-0 z-10 flex px-8 min-h-16 w-full ${isScrolled ? 'bg-white dark:bg-[#0c0d0c] dark:bg-opacity-70' : 'bg-transparent'}`}>
-      <div className="container mx-auto">
-        <div className="flex justify-between">
+      <div className="container mx-auto flex items-center">
+        <div className="flex justify-evenly items-center">
             {/* Section gauche */}
             <div className="flex-shrink-0 pr-14 lg:flex items-center ">
               <a className={`font-medium font-heading flex text-xl ${darkMode && isScrolled ? 'text-white' : 'text-black'}`} href="#">
@@ -52,77 +55,43 @@ const Header = () => {
               </button> */}
             </div>
 
-              {/* Bouton du menu burger (affiché uniquement sur les petits écrans) */}
-            <button className="navbar-burger lg:hidden self-center">
-              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><rect y="6" width="24" height="2" fill="white"></rect><rect y="11" width="24" height="2" fill="white"></rect><rect y="16" width="24" height="2" fill="white"></rect></svg>
-            </button>
+            {/* Bouton du menu burger (affiché uniquement sur les petits écrans) */}
+            <button className={`navbar-burger lg:hidden self-center ${darkMode && isScrolled ? 'text-white' : 'text-black'}`} onClick={() => setMenuOpen(!menuOpen)}>
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect y="0" width="24" height="4" fill="currentColor"></rect>
+                <rect y="8" width="24" height="4" fill="currentColor"></rect>
+                <rect y="16" width="24" height="4" fill="currentColor"></rect>
+            </svg>
+          </button>
         </div>
       </div>
-
-      <a href="#" className="fixed bottom-5 lg:bottom-10 right-5 lg:right-10 z-40"><button type="button" className="h-8 w-8 p-2 tracking-widest uppercase text-white font-light text-sm rounded-sm cursor-pointer transition-all bg-pink-500 hover:opacity-75"><svg fill="none" viewBox="0 0 24 24" className="h-full w-full"><path fill="currentColor" d="M17.657 16.243l1.414-1.414-7.07-7.072-7.072 7.072 1.414 1.414L12 10.586l5.657 5.657z"></path></svg></button></a>
     </nav>
 
-    <div className="hidden navbar-menu fixed top-0 left-0 bottom-0 w-5/6 max-w-sm z-50">
+    {/* Header section */}
+    <div className="px-4 flex flex-col text-center items-center justify-center lg:justify-end h-full">  {/* Ajustez h-[400px] selon la hauteur souhaitée */}
+        <p className="my-4 poppins-font uppercase font-bold font-stroke-header tracking-wider mx-auto pointer-events-none text-4xl lg:text-[80px]" data-aos="fade-up" data-aos-delay="200">Venez découvrir</p>
+        <p className="my-8 poppins-font uppercase font-bold font-stroke-header tracking-wider mx-auto pointer-events-none text-6xl lg:text-[130px]" data-aos="fade-up" data-aos-delay="200">La Baie de Somme</p>
+    </div>
+
+    <div className={menuOpen ? "navbar-menu fixed top-0 left-0 bottom-0 right-0 z-50" : "hidden"}>
       <div className="navbar-backdrop fixed inset-0 bg-gray-800 opacity-25"></div>
-      <nav className="relative flex flex-col py-8 px-10 w-full h-full bg-black border-r border-slate-500 overflow-y-auto">
-        <a className="inline-block text-xl text-white font-medium font-heading mb-16 md:mb-32" href="#">
-          <img className="h-8" width="auto" src="boldui-assets/logo/logo-boldui-light.svg" alt=""/>
-        </a>
-        <ul className="mb-32">
-          <li className="mb-10">
-            <a className="flex items-center" href="#">
-              <span className="mr-3 text-lg text-white">About</span>
-              <svg width="16" height="9" viewBox="0 0 16 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M12.01 3.48047H0V5.57278H12.01V8.71124L16 4.52663L12.01 0.34201V3.48047Z" fill="#FFEC3E"></path>
-              </svg>
-            </a>
-          </li>
-          <li className="mb-10">
-            <a className="flex items-center" href="#">
-              <span className="mr-3 text-lg text-white">Company</span>
-              <svg width="16" height="9" viewBox="0 0 16 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M12.01 3.48047H0V5.57278H12.01V8.71124L16 4.52663L12.01 0.34201V3.48047Z" fill="#FFEC3E"></path>
-              </svg>
-            </a>
-          </li>
-          <li className="mb-10">
-            <a className="flex items-center" href="#">
-              <span className="mr-3 text-lg text-white">Services</span>
-              <svg width="16" height="9" viewBox="0 0 16 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M12.01 3.48047H0V5.57278H12.01V8.71124L16 4.52663L12.01 0.34201V3.48047Z" fill="#FFEC3E"></path>
-              </svg>
-            </a>
-          </li>
-          <li>
-            <a className="flex items-center" href="#">
-              <span className="mr-3 text-lg text-white">Contact</span>
-              <svg width="16" height="9" viewBox="0 0 16 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M12.01 3.48047H0V5.57278H12.01V8.71124L16 4.52663L12.01 0.34201V3.48047Z" fill="#FFEC3E"></path>
-              </svg>
-            </a>
-          </li>
+      <nav className="relative flex flex-col items-center justify-center text-center text-3xl h-full bg-black bg-opacity-70 text-white">
+        <button  className="mb-4" onClick={() => setMenuOpen(false)}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 h-12">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+        </svg>
+        </button> {/* Fermer le menu */}
+        <ul className="space-y-4">
+          <li><a href="#gites" onClick={() => setMenuOpen(false)}>Gîtes</a></li>
+          <li><a href="#contacts" onClick={() => setMenuOpen(false)}>Contacts</a></li>
+          <li><a href="#location" onClick={() => setMenuOpen(false)}>Localisation</a></li>
+          <li><a href="#faq" onClick={() => setMenuOpen(false)}>FAQ</a></li>
+          <li><a href="#activities" onClick={() => setMenuOpen(false)}>Activités</a></li>
+          <li><a href="#partners" onClick={() => setMenuOpen(false)}>Partenaires</a></li>
         </ul>
-        <a className="flex mb-8 items-center justify-center py-4 px-6 rounded-full bg-yellow-300 hover:bg-yellow-400 transform duration-200" href="#">
-          <svg className="mr-3" width="16" height="9" viewBox="0 0 16 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M12.01 3.16553H0V5.24886H12.01V8.37386L16 4.20719L12.01 0.0405273V3.16553Z" fill="black"></path>
-          </svg>
-          <span className="text-sm font-medium uppercase tracking-wider">Sign Up</span>
-        </a>
-        <a className="flex mb-10 items-center text-white hover:underline" href="#">
-          <span className="mr-2 text-sm">Log In</span>
-          <svg width="16" height="10" viewBox="0 0 16 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M12.01 3.95383H0V6.04614H12.01V9.1846L16 4.99998L12.01 0.815369V3.95383Z" fill="#FFEC3E"></path>
-          </svg>
-        </a>
-        <p className="text-sm text-gray-500">All rights reserved © BoldUI 2021</p>
       </nav>
     </div>
 
-    {/* Header section */}
-    <div className="px-4 flex flex-col items-start justify-end h-[1000px]">  {/* Ajustez h-[400px] selon la hauteur souhaitée */}
-        <p className="poppins-font uppercase font-bold font-stroke-header tracking-wider lg:text-[90px] mx-auto pointer-events-none" data-aos="fade-up" data-aos-delay="200">Venez découvrir</p>
-        <p className="poppins-font uppercase font-bold font-stroke-header tracking-wider lg:text-[180px] mx-auto pointer-events-none" data-aos="fade-up" data-aos-delay="200">La Baie de Somme</p>
-    </div>
   </section>
   )
 }
